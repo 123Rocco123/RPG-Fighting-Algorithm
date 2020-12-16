2ef0a7e0a5194e5a6c38630e0321cfbd7723be04
123Rocco123 <63969485+123Rocco123@users.noreply.github.com>
2020-12-16 19:09:46 +0000

    Overhaul Commit
    The following commit contains various additions to the algorithm:
    1) The inclusion of a class system for the different character architypes
    
    2) Input system so that the players will be able to choose their levels and characters
    
    3) A random system for the occurance of a battle
    
    4) A class containing the code for the algorithm
    
    5) A "resistance" system for the enemies so as to make it that they're more resistant certain types of classes.
    
    6) Overhaul to the damage system dealt by the players

--- DnD Algorithm.py
+++ DnD Algorithm.py
@@ -1,63 +1,141 @@
 import random
 import time
 
-attack_dic = {melee_attacks : ["hit", "attack", "melee", "rage", "slash", "thrust"], spell_attacks : ["spell", "cast", "bewitch"]}
+#attack_dic = {melee_attacks : ["hit", "attack", "melee", "rage", "slash", "thrust"], spell_attacks : ["spell", "cast", "bewitch"]}
+end = False
+numbers = 0
 
-enemy_ac = " "
-level = 1
-bonus_dmg = level + random.randint(1,20)
+#The types of classes that the player can choose to be, 7 player maximum.
+magic_types = [
+  "magic",
+  "melee",
+  "ice",
+  "fire",
+  "electric",
+  "spirit",
+  "energy"
+]
 
-PLACEHOLDER = 4
+for x in magic_types:
+    numbers += 1
+    print(numbers, x)
 
-battle = False
+#This variable will keep track of how many plaers are in the game, and therefore, will be used to determine the characters in the game.
+how_many = int(input("How many players are there? "))
 
-enemy_grunt = 500
-enemy_health_boss = 1000
-big_bad = 5000
+#The dictionary below is used to keep track of each players damage type, and their level.
+players = {}
+player_count = 0
 
-danger = 1
+#The while loop here is used to record the player's character architypes. 
+while how_many > 0:
+    player_count += 1
+    #The list here is used to contain the attributes for a single character
+    player_attrib = []
+    
+    attrib1 = input("Choose your character's damage type: ")
+    attrib2 = input("What is your current level? ")
+
+    player_attrib.append(attrib1)
+    player_attrib.append(attrib2)
+    #This will then append the character's information to the dictionary for the specific player.
+    players[player_count] = player_attrib
+
+    how_many -= 1
+
+print(players)
+
+#enemy_ac = " "
+#level = 1
+#bonus_dmg = level + random.randint(1,20)
+
+#PLACEHOLDER = 4
+
+#battle = False
+
+#enemy_grunt = 500
+#enemy_health_boss = 1000
+#big_bad = 5000
+
+#danger = 1
+
+#grunt_dmg = level * danger
 
 
-def algorithm():
-    time.sleep(600)
-    while end != True:
-        random.randint(1, 10)
-        fight = 0
+class algorithm:
+    def __init__(self, type1, level, power, resistance):
+        self.type1 = type1
+        self.level = level
+        self.power = power
+        self.resistance = resistance
 
-        fight += random.randint(1,10)
-        if fight >= 7:
-            #Enemy choice and health of the enemy horde.
-            rand_bigbad = random.ranint(1,4)
-            if rand_bigbad == 1:
-                horde = danger * enemy_grunt 
-            elif rand_bigbad == 2:
-                horde = (danger / 2) * enemy_heath_boss
-            elif rand_bigbad == 3:
-                horde = (danger * enemy_grunt) + (danger/2 * enemy_health_boss)
+    def resistance(self):
+        #This function contains the code that will be randomly assigned to the hordes when the players have to fight them.
+            #The hordes will be randomly resistant to 1 of the 7 magic types, giving emphesis on the players to have a diverse cast of characters. 
+        resist = random.randint(1,7)
+        if resist == 1:
+            magic_dmg -= 10 * self.level
+        elif resist == 2:
+            melee_dmg -= 10 * self.level
+        elif resist == 3:
+            ice_dmg -= 10 * self.level
+        elif resist == 4:
+            fire_dmg -= 10 * self.level
+        elif resist == 5:
+            electric_dmg -= 10 * self.level
+        elif resist == 6:
+            spirit_dmg -= 10 * self.level
+        elif resist == 7:
+            energy_dmg -= 10 * self.level
 
-            #Damage from the players to the horde.
-            if bonus_dmg * level >= horde:
-                print("You've successfully killed off the entire horde!")
-                EnemyHealth = EnemyHealth - (bonus_dmg + possibility)
-                if EnemyHealth <= 0:
-                    print("You've defeated the enemy!")
+    def enemy_move(self):
+        attack_who = random.ranint(1, self.amount_of_players)
+
+
+
+    def algorithm(self):
+        time.sleep(600)
+        while end != True:
+            random.randint(1, 10)
+            fight = 0
+
+            fight += random.randint(1,10)
+            if fight >= 7:
+                #Enemy choice and health of the enemy horde.
+                rand_bigbad = random.ranint(1,4)
+                if rand_bigbad == 1:
+                    horde = danger * enemy_grunt 
+                elif rand_bigbad == 2:
+                    horde = (danger / 2) * enemy_heath_boss
+                elif rand_bigbad == 3:
+                    horde = (danger * enemy_grunt) + (danger/2 * enemy_health_boss)
+
+                #Damage from the players to the horde.
+                if bonus_dmg * level >= horde:
+                    print("You've successfully killed off the entire horde!")
+                    EnemyHealth = EnemyHealth - (bonus_dmg + possibility)
+                    if EnemyHealth < horde:
+                        kills = horde % (level*bonus_dmg)
+                        horde -= level*bonus_dmg
+                        print("You've killed", kills, "enemies.")
+                        enemy_move()
+                else:
+                    print("You we're unable to pull of the move.")
+
+                if battle == True:
+                    move = input("What move are you gonna do?")
+
+                    move_words = move.split()
+
+                    for x in move_words:
+                        for y in keywords:
+                            if x == y:
+                                possibility = random.randint(1,20)
+                                damage(PLACEHOLDER)
+                time.sleep(450)
+                fight = 0
+                algorithm()
             else:
-                print("You we're unable to pull of the move.")
-
-            if battle == True:
-                move = input("What move are you gonna do?")
-
-                move_words = move.split()
-
-                for x in move_words:
-                    for y in keywords:
-                        if x == y:
-                            possibility = random.randint(1,20)
-                            damage(PLACEHOLDER)
-            time.sleep(450)
-            fight = 0
-            algorithm()
-        else:
-            time.sleep(300)
-            fight = 0
-            algorithm()
+                time.sleep(300)
+                fight = 0
+                algorithm()
